2d91c45bc0a831a68202772df5a3da1fc4eec866
Elias Bruhn <elias@lfpost.dk>
2021-04-27 18:05:12 +0200

    Something

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function (req, res) {
+  app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server is running on port ' + port);
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
@@ -34,9 +34,9 @@ app.post('/payment', (req, res) => {
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
-      res.status(500).send({error:stripeErr})
+      res.status(500).send({ error: stripeErr });
     } else {
-      res.status(200).send({success: stripeRes})
+      res.status(200).send({ success: stripeRes });
     }
-  })
+  });
 });
